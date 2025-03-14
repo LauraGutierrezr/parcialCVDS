357955210b1558aba546c07ed07c6470d92657ef
LauraGutierrezr <114092766+LauraGutierrezr@users.noreply.github.com>
2025-03-13 22:36:49 -0500

    Delete parcialCvds2025-1 directory

--- parcialCvds2025-1/readMe.md
+++
@@ -1,1 +0,0 @@
-1. ![image](https://github.com/user-attachments/assets/b765781f-480d-4cd8-b549-cb98df009194)
--- parcialCvds2025-1/src/main/java/eci/edu/cvds/parcialCVDS25_1/Agents/Agente.java
+++
@@ -1,7 +0,0 @@
-package eci.edu.cvds.parcialCVDS25_1.Agents;
-
-public interface Agente {
-
-    public void notificacion(String name, int stock);
-    
-}
